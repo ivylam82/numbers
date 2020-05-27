8f9e2b431841ee9c5371ee7863f0e39e32b59b08
Ivy Lam <ivylam82@hotmail.com>
2020-05-27 21:33:38 +0800

    third version

--- numbers.py
+++ numbers.py
@@ -4,13 +4,17 @@
 # 猜錯的話 要告訴他比答案大或少
 import random
 answer = random.randint (1, 100)
+count = 0
 while True:
+	count = count + 1
 	num = input ('請猜數字? ')
 	num = int(num)
 	if num == answer:
 		print ('終於猜對了')
+		print ('你猜了', count, '次' )
 		break
 	elif num > answer:
 		print ('數字比較小啊! ')
 	elif num < answer:
 		print ('數字比較大啊! ')
+	print ('你猜了', count, '次' )
